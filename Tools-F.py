2fd2dd1b112598d253b0ff93de5fd08a4b15cee1
xxoprt <86563093+xxoprt@users.noreply.github.com>
2021-07-30 22:19:33 +0700

    Add files via upload

---
+++ Tools-F.py
@@ -0,0 +1,218 @@
+#non Recode
+#code by MR.FAGHP
+import os
+from time import sleep
+
+os.system('clear')
+
+sleep(3)
+print '          nomor 1 itu tools ny           '
+print '========================================='
+print '|(1). DDoS,CrackFb,Hackcctv,Virus,onex  |'
+print '|(2). Exit                              |'
+print '========================================='
+print '|Author         : MR.FAGHP BLACK-404/F |'
+print '|Pembuat Tools-F: MR.FAGHP BLACK-404/F |'
+print '========================================'
+
+pilih = input('====> ')
+
+if pilih == 1:
+     print '                DDOS PILIHAN                '
+     print '============================================'
+     print '(2) ddosWD'
+     print '(3) MCIDDOS'
+     print '(4) LITEDDOS'
+     print '(5) CrackFb'
+     print '==========================================='
+
+pilih = input('====> ')
+
+if pilih == 2:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install python')
+     os.system('pkg install python2')
+     os.system('pkg install bash')
+     os.system('pkg install git')
+     os.system('git clone https://github.com/banghyuu/ddosWD')
+     os.system('cd ddosWD')
+     os.system('sh Ddos.sh')
+
+if pilih == 3:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.systen('pkg install git')
+     os.system('pkg install python && pkg install python2')
+     os.system('pkg install php && pkg install curl')
+     os.system('pip2 install netaddr')
+     os.system('pip install netaddr')
+     os.system('git clone https://github.com/MrTamfanX/MCIDDOS')
+     os.system('cd MCIDDOS')
+     os.system('python2 TamfanDdos.py')
+
+if pilih == 4:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.systen('pkg install git')
+     os.system('pkg install python && pkg install python2')
+     os.system('pkg install php && pkg install curl')
+     os.system('pip2 install netaddr')
+     os.system('pip install netaddr')
+     os.system('git clone https://github.com/4L13199/LITEDDOS')
+     os.system('cd LITEDDOS')
+     os.system('python2 LITEDDOS.py')
+
+elif pilih == 5:
+       print '             CrackFb Pilihan           '
+       print '========================================'
+       print '(6) Premium'
+       print '(7) simbf'
+       print '(8) ymbf'
+       print '(9) Hackcctv'
+       print '========================================'
+
+pilih = input('====> ' )
+
+if pilih == 6:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('termux-setup-storage')
+     os.system('pkg install python ')
+     os.system('pkg install git')
+     os.system('pip install bs4')
+     os.system('pip install requests')
+     os.system('pip install mechanize')
+     os.system('pip install futures')
+     os.system('rm -rf premium')
+     os.system('git clone https://github.com/Dapunta/premium')
+     os.system('cd premium')
+     os.system('python premium.py')
+
+if pilih == 7:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install git')
+     os.system('pkg install python')
+     os.system('pkg install python2')
+     os.system('pkg install jq -y')
+     os.system('pip2 install requests')
+     os.system('pip2 install mechanize')
+     os.system('pip2 install tqdm')
+     os.system('pip2 install bs4')
+     os.system('pip2 install colorama')
+     os.system('git clone https://github.com/Ibnustagnz/simbf')
+     os.system('cd simbf')
+     os.system('git pull')
+     os.system('python2 run.py')
+
+if pilih == 8:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install python2')
+     os.system('pkg install git')
+     os.system('git clone https://github.com/Yayan-XD/ymbf')
+     os.system(' pip2 install requests bs4')
+     os.system('pip2 install futures')
+     os.system('cd ymbf')
+     os.system('python2 ymbf.py')
+
+elif pilih ==  9:
+       print '         hack cctv pilihan       '
+       print '==================================='
+       print '(10)ipcs'
+       print '(11)Cam-Hackers'
+       print '(12)Virus'
+       print '=================================='
+
+pilih = input('====> ' )
+
+if pilih == 10:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install python2 -y')
+     os.system('pkg install git -y')
+     os.system('git clone https://github.com/Kancotdiq/ipcs')
+     os.system('cd ipcs')
+     os.system('pip2 install requests')
+     os.system('python2 scan.py')
+
+if pilih == 11:
+     print 'Tunggu'
+     os.system('apt-get install python3')
+     os.system('apt-get install git')
+     os.system('git clone https://github.com/AngelSecurityTeam/Cam-Hackers')
+     os.system('pip3 install requests')
+     os.system('pip3 install colorama')
+     os.system('cd Cam-Hackers')
+     os.system('python3 cam-hackers.py')
+
+elif pilih == 12:
+       print '             Virus pilihan               '
+       print '========================================='
+       print '(13)virtex'
+       print '(14)link-virus'
+       print '(15)vbug'
+       print '(16) onex'
+       print '========================================='
+
+pilih = input('====> ' )
+
+if pilih == 13:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install bash')
+     os.system('pkg install figlet')
+     os.system('pkg install git')
+     os.system('git clone https://github.com/MrVirusSpm-07/virtex')
+     os.system('cd virtex')
+     os.system('sh virtex-x.sh')
+
+if pilih == 14:
+    print 'Tunggu'
+    os.system('pkg update && pkg upgrade')
+    os.system('pkg install bash')
+    os.system('pkg install figlet')
+    os.system('pkg install git')
+    os.system('git clone https://github.com/MrVirusSpm-07/link-virus')
+    os.system('cd link-virus')
+    os.system('sh download.sh')
+
+if pilih == 15:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install git')
+     os.system('pkg install python')
+     os.system('pkg install python2')
+     os.system('termux-setup-storage')
+     os.system('git clone https://github.com/Gameye98/vbug')
+     os.system('cd vbug')
+     os.system('python2 vbug.py')
+
+elif pilih == 16:
+      print '           Ini Tools Pilihan          '
+      print '======================================='
+      print '(17) install'
+      print '(18) Exit'
+      print '======================================='
+
+pilih = input('====> ' )
+
+if pilih == 17:
+      print 'Tunggu'
+      os.system('apt update')
+      os.system('apt upgrade')
+      os.system('apt install git')
+      os.system('apt install ruby')
+      os.system('gem install lolcat')
+      os.system('apt install python')
+      os.system('apt install python2')
+      os.system('apt install jq -y')
+      os.system('apt install bash')
+      os.system('apt-get install python3')
+      os.system('git clone https://github.com/rajkumardusad/onex')
+      os.system('cd onex')
+      os.system('chmod 777 install')
+      os.system('chmod 777 onex')
+      os.system('./install')
+      os.system('onex start')
